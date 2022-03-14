7c21f42dc6195c430f875add453371880574d4a5
Taylor Beseda <tbeseda@gmail.com>
2022-03-14 13:33:30 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+A sample [Architect](https://arc.codes) app with [`@architect/plugin-remix`](https://www.npmjs.com/package/@architect/plugin-remix).
--- src/http/get-hello/index.js
+++ src/http/get-hello/index.js
@@ -1,6 +1,10 @@
 exports.handler = async () => {
   return {
     statusCode: 200,
-    body: 'hello'
+    headers: { 'content-type': 'text/html; charset=utf8' },
+    body: `
+<h1>Hello 👋</h1>
+<h2><a href="/">Go to root /</a></h2>
+    `.trim()
   }
 }
